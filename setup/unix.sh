6ec8720660e1ebe9ddd686ccbacfd292813e4c79
Samuel <samuelteodoziolima2010@gmail.com>
2025-11-12 14:21:23 -0300

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# Setup script for Android
+# Setup script for macOS and Linux
 cd ..
 echo "Setting up Haxe libraries..."
 
-mkdir ~/haxelib && haxelib setup ~/haxelib
+mkdir -p ~/haxelib && haxelib setup ~/haxelib
 
 echo "Installing dependencies via HMM..."
 echo "This might take a few moments depending on your internet speed."
 
-# Install and run your custom HMM
+# Install and run your custom HMM fork
 haxelib git hmm https://github.com/SatoruSam/hmm --quiet
 haxelib run hmm install --quiet
 
